4fccee86dfe74b98634de803ad4dfea624b2164e
ThreePounds <mail@tobiashuth.de>
2023-04-05 15:26:05 +0200

    adding tkinter as interface and sqlite3 as db engine

---
+++ main.py
@@ -0,0 +1,35 @@
+# --- imports ---
+import tkinter as tk
+import sqlite3
+import os.path
+
+# --- constants ---
+DATABASEFILE = 'examfile.db'
+
+# --- interface ---
+
+root = tk.Tk()
+
+# --- initialize database ---
+
+if os.path.isfile(DATABASEFILE):
+    pass
+else:
+    #setting up data base file
+    con = sqlite3.connect(DATABASEFILE)
+    cur = con.cursor()
+    cur.execute(
+        "CREATE TABLE employee("
+            "name," 
+            "givenname,"
+            "id," # Lfd-Nummer
+            "archived," # 1 wenn archiviert
+            "hidden," # 1 wenn versteckt
+            "birthyear,"
+        ")"
+    )
+    con.commit()
+    con.close()
+    
+# --- main event loop ---
+root.mainloop()
